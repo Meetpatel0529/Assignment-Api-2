d89cecd064a8808140ede957c63427ac97fa9047
Meetpatel0529 <meetp8608@gmail.com>
2025-02-22 15:25:13 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@
  */
 
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
